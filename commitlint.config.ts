0c4dd6561b58144281189e10c522342dfbb6b91b
Arrizo <arrizo787392502@163.com>
2023-05-14 12:44:36 +0800

    新增项目

---
+++ commitlint.config.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: chenzechao
+ * @Date: 2023-05-14 12:25:44
+ * @LastEditTime: 2023-05-14 12:25:55
+ * @LastEditors: chenzechao
+ * @Description: 
+ * @FilePath: /tius/tius-manager-system/commitlint.config.ts
+ */
+module.exports = {
+  extends: [
+    'cz'
+  ]
+};
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: chenzechao
+ * @Date: 2023-05-14 12:08:24
+ * @LastEditTime: 2023-05-14 12:37:33
+ * @LastEditors: chenzechao
+ * @Description: 
+ * @FilePath: /tius/tius-manager-system/src/App.vue
+-->
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
