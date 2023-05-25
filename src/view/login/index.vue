8502eb001ee2516f02a6575e61f1dd3468aa6683
chenzechao <chenzc@jw99.net>
2023-05-25 14:55:49 +0800

    f

--- src/view/login/index.vue
+++ src/view/login/index.vue
@@ -1,13 +1,13 @@
 <!--
  * @Author: chenzechao
  * @Date: 2023-05-25 00:01:52
- * @LastEditTime: 2023-05-25 00:02:31
- * @LastEditors: chenzechao
+ * @LastEditTime: 2023-05-25 14:55:42
+ * @LastEditors: chenzechao chenzc@jw99.net
  * @Description: 
  * @FilePath: /tius-manager-system/src/view/login/index.vue
 -->
 <template>
-  <div></div>
+  <div>d</div>
 </template>
 <script lang="ts" setup>
 </script>
