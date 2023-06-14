93cf5259fd21e7163314f15f1c7c9c5298d7e3ce
Arrizo <arrizo787392502@163.com>
2023-06-14 22:08:25 +0800

    d

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <!--
  * @Author: chenzechao
  * @Date: 2023-05-14 12:08:24
- * @LastEditTime: 2023-05-25 00:09:38
+ * @LastEditTime: 2023-06-14 22:08:16
  * @LastEditors: chenzechao
  * @Description: 
  * @FilePath: /tius-manager-system/src/App.vue
 -->
 <template>
+  
   <router-view></router-view>
 </template>
