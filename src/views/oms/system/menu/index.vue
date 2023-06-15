ce96eaef761afbe9020eb67e4cdb832e3f2e5a45
chenzechao <chenzc@jw99.net>
2023-06-15 16:20:30 +0800

    修复release分支bug

--- src/views/oms/system/menu/index.vue
+++ src/views/oms/system/menu/index.vue
@@ -2,12 +2,16 @@
  * @Author: chenzechao chenzc@jw99.net
  * @Date: 2023-06-05 09:02:24
  * @LastEditors: chenzechao chenzc@jw99.net
- * @LastEditTime: 2023-06-15 16:12:37
+ * @LastEditTime: 2023-06-15 16:20:16
  * @FilePath: \tius-manager-system\src\views\oms\system\menu\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  <div>mene-修复release-bug</div>
+<div>
+  1、拉取分支
+  2、修复bug
+  3、重新合并到release分支
+</div>
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
