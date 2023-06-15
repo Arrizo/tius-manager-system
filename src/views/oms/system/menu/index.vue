f8c0a3593b2ab9f7da98280d240a133deb76a6ef
chenzechao <chenzc@jw99.net>
2023-06-15 16:17:45 +0800

    修复release分支bug

--- src/views/oms/system/menu/index.vue
+++ src/views/oms/system/menu/index.vue
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  <div></div>
+  <div>mene-修复release-bug</div>
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
