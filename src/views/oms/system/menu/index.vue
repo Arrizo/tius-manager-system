57f155a66a3866c7f4576269a71366bc899b1192
chenzechao <chenzc@jw99.net>
2023-06-15 16:13:21 +0800

    新功能模块开发

--- src/views/oms/system/menu/index.vue
+++ src/views/oms/system/menu/index.vue
@@ -1,1 +1,14 @@
-<template>menu</template>
+<!--
+ * @Author: chenzechao chenzc@jw99.net
+ * @Date: 2023-06-05 09:02:24
+ * @LastEditors: chenzechao chenzc@jw99.net
+ * @LastEditTime: 2023-06-15 16:12:37
+ * @FilePath: \tius-manager-system\src\views\oms\system\menu\index.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div></div>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+</script>
